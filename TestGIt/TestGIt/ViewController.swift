00336d837b343bdc05e8bef45956a497837b6933
michael <407677069@qq.com>
2015-08-06 15:21:49 +0800

    新加入code

--- TestGIt/TestGIt/ViewController.swift
+++ TestGIt/TestGIt/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // new add code
         // Do any additional setup after loading the view, typically from a nib.
     }
 
